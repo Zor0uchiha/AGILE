a5ee0c9b0d11c2cb2ffde4e34cac705561d0a2b0
unknown <exam7@pro16.cse.com>
2025-06-04 22:29:43 -0700

    initial commit agile lab

---
+++ Hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num1, num2, sum;
+    printf("\t\tAddition Sample Agile Program\n");
+    printf("\nEnter number 1: "):
+    scanf("%d",&num1);
+    printf("\nEnter number 2 : ");
+    scanf("%d",&num2);
+    sum=num1+num2;
+    printf("\nSum of %d and %d is : %d",num1,num2,sum);
+    return 0;
+}
---
+++ main.py
@@ -0,0 +1,5 @@
+print("\n\n\tAddition sample agile python code\n\n")
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+sum=num1+num2
+print("The sum of",num1,"and",num2,": ",sum)
